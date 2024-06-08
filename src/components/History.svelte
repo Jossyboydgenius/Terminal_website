e18956c5a110160532910f23590166639ddf04b0
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-06-08 17:20:28 +0000

    feat: Add History component for displaying command history

---
+++ src/components/History.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { history } from '../stores/history';
+  import { theme } from '../stores/theme';
+  import Ps1 from './Ps1.svelte';
+</script>
+
+{#each $history as { command, outputs }}
+  <div style={`color: ${$theme.foreground}`}>
+    <div class="flex flex-col md:flex-row">
+      <Ps1 />
+
+      <div class="flex">
+        <p class="visible md:hidden">❯</p>
+
+        <p class="px-2">{command}</p>
+      </div>
+    </div>
+
+    {#each outputs as output}
+      <p class="whitespace-pre">
+        {output}
+      </p>
+    {/each}
+  </div>
+{/each}

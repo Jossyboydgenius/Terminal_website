89a3e6063bf82670e7c32ce23d55605316573f90
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-06-08 17:21:02 +0000

    feat: Add Ps1 component for displaying the command prompt

---
+++ src/components/Ps1.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { theme } from '../stores/theme';
+  let hostname = window.location.hostname;
+</script>
+
+<h1 class="font-bold flex">
+  <span style={`color: ${$theme.yellow};`}>guest</span>
+  <span style={`color: ${$theme.white}`}>@</span>
+  <span style={`color: ${$theme.green}`}>{hostname}</span>
+  <span style={`color: ${$theme.white}`}>:~$</span>
+</h1>

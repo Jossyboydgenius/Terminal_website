1a511343b59fe9a8c69adf5a062ec8d80816b107
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-06-08 17:20:08 +0000

    feat: Add lazy loading for images in App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Ps1 from './components/Ps1.svelte';
+  import Input from './components/Input.svelte';
+  import History from './components/History.svelte';
+  import { theme } from './stores/theme';
+</script>
+
+<svelte:head>
+  {#if import.meta.env.VITE_TRACKING_ENABLED === 'true'}
+    <script
+      async
+      defer
+      data-website-id={import.meta.env.VITE_TRACKING_SITE_ID}
+      src={import.meta.env.VITE_TRACKING_URL}
+    ></script>
+  {/if}
+</svelte:head>
+
+<main
+  class="h-full border-2 rounded-md p-4 overflow-auto text-xs sm:text-sm md:text-base"
+  style={`background-color: ${$theme.background}; color: ${$theme.foreground}; border-color: ${$theme.green};`}
+>
+  <History />
+
+  <div class="flex flex-col md:flex-row">
+    <Ps1 />
+
+    <Input />
+  </div>
+</main>
